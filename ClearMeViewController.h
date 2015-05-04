73b25573f1d63c35a8b6b117c7cfceecac0cc93d
Justin Huntington <corusco@gmail.com>
2015-05-04 11:35:14 -0600

    fix this

---
+++ ClearMeViewController.h
@@ -0,0 +1,13 @@
+//
+//  ClearMeViewController.h
+//  ClearMe
+//
+//  Created by Justin Huntington on 5/4/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ClearMeViewController : UIViewController
+
+@end
---
+++ ClearMeViewController.m
@@ -0,0 +1,37 @@
+//
+//  ClearMeViewController.m
+//  ClearMe
+//
+//  Created by Justin Huntington on 5/4/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import "ClearMeViewController.h"
+
+@interface ClearMeViewController ()
+
+@end
+
+@implementation ClearMeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
